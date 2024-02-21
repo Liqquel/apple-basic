305ee8111e35adbd7c3234be670bbcff0e6c9454
Maliqul Jibran <jibranajaa@gmail.com>
2024-02-21 11:33:51 +0700

    sample commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript untuk interaktivitas di halaman Apple Store
+
+// Contoh JavaScript interaktif
+console.log("Halaman web Apple Store sedang dimuat...");
+
+// You can add more JavaScript functionalities as needed.
